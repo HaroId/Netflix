cf18c6351e8c8807728330473e47a17c22578b0c
HaroId <harolewu@gmail.com>
2022-09-27 00:37:30 -0500

    update images

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
+
+//---------------------lista de imagenes-------------------------
+var imagenes = [ 'vi-arcane.jpg', 'detective-pikachu.jpg', 'gods-of-war.jpg', 'Kimetsu no Yaiba.jpg', 'myers.jpg', 'pennywise.jpg', 'rick-and-morty.jpg', 'star-wars.jpg', 'abominables.jpg', 'free-guy.jpg' ]
+
+const carousel = document.getElementById('carousel');
+
+const imagen = imagenes[0]
+
+imagenes.forEach((imagen) => {
+    const div = document.createElement('div');
+    div.classList.add('pelicula');
+    const img = document.createElement('img');
+    img.src = `img/${imagen}`;
+    div.appendChild(img);
+    carousel.appendChild(div);
+});
+
+//-------------Constantes de Animacion-------------------------
+
 const fila = document.querySelector('.contenedor-carousel');
 const peliculas = document.querySelectorAll('.pelicula');
 
 const flechaizquierda = document.getElementById('flecha-izquierda');
 const flechaderecha = document.getElementById('flecha-derecha');
+
+//--------------cambio de background--------------------------
+
+
+
 
 //-------------event Listener para la flecha derecha----------
 
@@ -46,6 +70,7 @@ for(let i = 0;i < numeroPaginas; i++){
     })
 }
 
+
 //------------------------mover-----------------------------------
 peliculas.forEach((pelicula) => {
     pelicula.addEventListener('mouseenter', (e) => {
@@ -60,25 +85,3 @@ peliculas.forEach((pelicula) => {
 fila.addEventListener('mouseleave', () => {
     peliculas.forEach(pelicula => pelicula.classList.remove('hover'));
 });
-
-//---------------------lista de imagenes-------------------------
-
-// var imagenes = [
-//     'img/vi-arcane.jpg',
-//     'img/detective-pikachu.jpg',
-//     'img/gods-of-war.jpg',
-//     'img/Kimetsu no Yaiba.jpg',
-//     'img/myers.jpg',
-//     'img/pennywise.jpg',
-//     'img/rick-and-morty.jpg',
-//     'img/star-wars.jpg',
-//     'amg/abominables.jpg',
-//     'img/free-guy.jpg',
-// ]
-
-// function carousel(contenedor){
-//     contenedor.addEventListener('click', e =>){
-
-//     }
-
-// }
